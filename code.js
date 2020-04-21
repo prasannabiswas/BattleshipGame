b00fc9450ae3481f1f6d840a64b52793c052360b
Prasanna Biswas <probiswas82@gmail.com>
2020-04-21 21:17:29 +0530

    Main code complete tested

--- code.js
+++ code.js
@@ -1,38 +1,49 @@
 var model = {
-boardSize: 7,
-numShips: 3,
-shipsSunk: 0,
-shipLength: 3,
+         boardSize: 7,
+         numShips: 3,
+         shipsSunk: 0,
+         shipLength: 3,
+
+
 ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
-{ locations: ["24", "34", "44"], hits: ["", "", ""] },
-{ locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+         { locations: ["24", "34", "44"], hits: ["", "", ""] },
+         { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+
+
 fire: function(guess) {
-for (var i = 0; i < this.numShips; i++) {
-var ship = this.ships[i];
-var index = ship.locations.indexOf(guess);
-if (index >= 0) {
-ship.hits[index] = "hit";
-view.displayHit(guess);
-view.displayMessage("HIT!");
-if (this.isSunk(ship)) {
-view.displayMessage("You sank my battleship!");
-this.shipsSunk++;
-}
-return true;
-}
-}
-view.displayMiss(guess);
-view.displayMessage("You missed.");
-return false;
+     for (var i = 0; i < this.numShips; i++) 
+     {
+       var ship = this.ships[i];
+       var index = ship.locations.indexOf(guess);
+       if (index >= 0) 
+       {
+            ship.hits[index] = "hit";
+            view.displayHit(guess);
+            view.displayMessage("HIT!");
+            if (this.isSunk(ship)) 
+             {
+               view.displayMessage("You sank my battleship!");
+               this.shipsSunk++;
+             }
+             return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
 },
-isSunk: function(ship) {
-for (var i = 0; i < this.shipLength; i++) {
-if (ship.hits[i] !== "hit") {
-return false;
-}
-}
-return true;
-}
+
+isSunk: function(ship) 
+{
+ for (var i = 0; i < this.shipLength; i++) 
+ {
+   if (ship.hits[i] !== "hit") 
+   {
+     return false;
+    }
+  }
+  return true;
+  }
 };
 
 
@@ -61,14 +72,3 @@ var view = {
 	},
 };	
 
-
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10");
