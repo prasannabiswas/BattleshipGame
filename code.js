15e985af940a2ad1e4f4d99adacccb92e514ab96
Prasanna Biswas <probiswas82@gmail.com>
2020-04-21 19:46:30 +0530

    Structure of the code

--- code.js
+++ code.js
@@ -1,3 +1,46 @@
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+    ships: [{locations: ["10","20","30"],hits:["", "", ""]},
+            [{locations: ["32","33","34"],hits:["", "", ""]},
+            [{locations: ["63","64","65"],hits:["", "", "hit"]}],
+
+    fire: function(guess)
+    {
+    	for(var i=0;i<this.numShips;i++)
+    	{
+    		var ship=this.ships[i];
+    		var index=ship.locations.indexOf(guess);
+    		if(index>=0)
+    		{
+    			ship.hits[index]="hit";
+    			if(this.isSunk(ship))
+    			{
+    				this.shipsSunk++;
+    			}
+    			return true;
+    		}
+    	}
+    	return false;
+    },
+
+    isSunk: function(ship)
+    {
+    	for(var i=0;i<this.shipLength;i++)
+    	{
+    		if(ship.hits[i]!=="hit")
+    		{
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+};
+
+
 var view = {
 	
 	displayMessage: function(msg)
@@ -23,6 +66,3 @@ var view = {
 	},
 };	
 
-view.displayMessage("tap tap!, is this thing on?");
-view.displayMiss("02");
-view.displayHit("26");
