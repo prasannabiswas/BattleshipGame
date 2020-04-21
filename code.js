f81d5c96735a46956a408b9c8c0adcce256ff133
Prasanna Biswas <probiswas82@gmail.com>
2020-04-21 21:55:38 +0530

    Convert alphabet to number in input

--- code.js
+++ code.js
@@ -1,3 +1,42 @@
+var controller = {
+	guesses:0,
+
+	processGuess: function(guess)
+	{
+
+	}
+
+};
+
+function parseGuess(guess) 
+{
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if (guess === null || guess.length !== 2) {
+  alert("Oops, please enter a letter and a number on the board.");
+}
+ else 
+    {
+      firstChar = guess.charAt(0);
+      var row = alphabet.indexOf(firstChar);
+      var column = guess.charAt(1);
+      if (isNaN(row) || isNaN(column)) 
+      {
+        alert("Oops, that isn't on the board.");
+      } 
+      else if (row < 0 || row >= model.boardSize ||
+               column < 0 || column >= model.boardSize) 
+      {
+        alert("Oops, that's off the board!");
+      }
+      else 
+      {
+        return row + column;
+      }
+    }
+    return null;
+}
+
+
 var model = {
          boardSize: 7,
          numShips: 3,
@@ -71,4 +110,5 @@ var view = {
 
 	},
 };	
+
 
